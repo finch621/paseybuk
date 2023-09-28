cc858cfe9432d81c209ea63463958c74d8c2b2b8
doffi <yongco.doffinyl@gmail.com>
2023-09-28 15:22:57 +0800

    proper solution for code generation and linking of schema file

--- types/schema-graphql/src/lib/generate-typings.ts
+++ types/schema-graphql/src/lib/generate-typings.ts
@@ -1,10 +1,16 @@
-import { GraphQLDefinitionsFactory } from '@nestjs/graphql';
+import { GenerateOptions, GraphQLDefinitionsFactory } from '@nestjs/graphql';
 import { join } from 'path';
 
-const definitionsFactory = new GraphQLDefinitionsFactory();
-definitionsFactory.generate({
+const options: GenerateOptions = {
   typePaths: ['./schema.graphql'],
   path: join(process.cwd(), 'src/lib/schema.graphql.ts'),
   outputAs: 'class',
   watch: true,
-});
+};
+
+if (process.env['NODE_ENV'] === 'production') {
+  options.watch = false;
+}
+
+const definitionsFactory = new GraphQLDefinitionsFactory();
+definitionsFactory.generate(options);
--- types/schema-graphql/src/lib/link-schema-to-server.ts
+++ types/schema-graphql/src/lib/link-schema-to-server.ts
@@ -1,15 +1,20 @@
-import { symlink } from 'fs';
+import { readFileSync, symlink } from 'fs';
 import { join } from 'path';
 
 const schemaFileName = 'schema.graphql';
-const serverProjectRoot = join(process.cwd(), '../../../../apps/server/src');
+const serverProjectRoot = join(process.cwd(), '../../apps/server/src');
 const serverSchemaLinkPath = `${serverProjectRoot}/${schemaFileName}`;
-const schemaPath = join(process.cwd(), `../../${schemaFileName}`);
+const schemaPath = join(process.cwd(), `./src/${schemaFileName}`);
 
-symlink(schemaPath, serverSchemaLinkPath, 'file', (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(`Symlink created to ${serverSchemaLinkPath}`);
-  }
-});
+if (!readFileSync(serverSchemaLinkPath)) {
+  symlink(schemaPath, serverSchemaLinkPath, 'file', (err) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    } else {
+      console.info(`Schema file linked to ${serverSchemaLinkPath}`);
+    }
+  });
+} else {
+  console.info(`Link already existed`);
+}

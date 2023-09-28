b9382f52304c7a552d13fa9d76d58f6b450a0a78
doffi <yongco.doffinyl@gmail.com>
2023-09-29 06:26:45 +0800

    refactor to accept shouldWatch args

--- types/schema-graphql/src/lib/generate-typings.ts
+++ types/schema-graphql/src/lib/generate-typings.ts
@@ -1,16 +1,14 @@
 import { GenerateOptions, GraphQLDefinitionsFactory } from '@nestjs/graphql';
 import { join } from 'path';
 
+const shouldWatch = process.argv[2];
+
 const options: GenerateOptions = {
   typePaths: [join(process.cwd(), 'src/schema.graphql')],
   path: join(process.cwd(), 'src/lib/schema.graphql.ts'),
   outputAs: 'class',
-  watch: true,
+  watch: shouldWatch ? true : false,
 };
-
-if (process.env['NODE_ENV'] === 'production') {
-  options.watch = false;
-}
 
 const definitionsFactory = new GraphQLDefinitionsFactory();
 definitionsFactory.generate(options);
